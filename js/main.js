468a9a4059dcb479ed74957af8d3324eddcda780
Tkaldahl <tkaldahl90@gmail.com>
2018-05-04 18:32:11 -0400

    added checkForMatch and flipCard functions to game logic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 var cards = ['queen', 'king', 'king', 'queen'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[3];
 
-cardsInPlay.push(cardOne);
-console.log('User flipped ' + cardsInPlay[0]);
-cardsInPlay.push(cardTwo);
-console.log('User flipped ' + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert('You found a match!');
-	}
-	else {
-		alert('Sorry, try again.');
+var checkForMatch = function(){
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert('You found a match!');
+		}
+		else {
+			alert('Sorry, try again.');
+		}
 	}
 }
+
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(3);
