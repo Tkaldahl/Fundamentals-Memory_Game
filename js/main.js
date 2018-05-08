69633941f1766ff2c471a25889fd3febdc4fe550
Tkaldahl <tkaldahl90@gmail.com>
2018-05-08 10:59:54 -0400

    Created createBoard function and modified flipCard & checkForMatch functions

--- js/main.js
+++ js/main.js
@@ -24,24 +24,39 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function(){
-	if (cardsInPlay.length === 2) {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert('You found a match!');
-		}
-		else {
-			alert('Sorry, try again.');
-		}
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+		cardsInPlay.pop();
+		cardsInPlay.pop()
+	}
+	else {
+		alert('Sorry, try again.');
+		cardsInPlay.pop();
+		cardsInPlay.pop()
 	}
 }
 
-var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
+
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	cardsInPlay.push(this.rank);
+	if (cardsInPlay.length === 2) {
+		checkForMatch()
+	}
+	else {
+		return
+	}
 }
 
-flipCard(0);
-flipCard(1);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
 
-console.log(cards[0].suite);
-console.log(cards[0].cardImage)
+createBoard()
